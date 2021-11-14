9cc32746a64639c25ce78ccafaa123c008d2c5e9
Rich <richard.a.cairns@gmail.com>
2021-11-14 11:35:35 +0000

    refactor so generation of chromatic scale is in its own func, and add test

--- fret.py
+++ fret.py
@@ -1,3 +1,5 @@
+from typing import List
+
 NUMBER_OF_FRETS = 12
 
 # Define the number of strings and the open pitch of each string. 1 is highest pitch.
@@ -9,17 +11,21 @@ tuning = {
     5: 'a',
     6: 'e',
 }
+def generate_chromatic_scale() -> List:
+    """
+    Return a list that represents the chromatic scale over three octaves. All enharmonic notes are shown as sharps.
+    """
+    chromatic = "a,a#,b,c,c#,d,d#,e,f,f#,g,g#"
+    chromatic = chromatic.split(",")
+    return chromatic * 3
 
-chromatic = "a,a#,b,c,c#,d,d#,e,f,f#,g,g#"
-chromatic = chromatic.split(",")
-chromatic = chromatic * 5
-
+chromatic_scale = generate_chromatic_scale()
 fretboard = {}
 for string in tuning.keys():
     open_string = tuning[string]
-    chromatic_index = chromatic.index(open_string.lower())
+    chromatic_index = chromatic_scale.index(open_string.lower())
     for fret in range(NUMBER_OF_FRETS + 1):  # Iteration 0 is the open string
-        note = chromatic[chromatic_index]
+        note = chromatic_scale[chromatic_index]
         fretboard[f"{string}{fret}"] = note
         chromatic_index += 1
 
--- test_fret.py
+++ test_fret.py
@@ -1,2 +1,7 @@
-def test_init():
-    assert 2 == 2
+from fret import generate_chromatic_scale
+
+def test_generate_chromatic_scale():
+    cs = generate_chromatic_scale()
+    assert cs == ['a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#']
+
+
