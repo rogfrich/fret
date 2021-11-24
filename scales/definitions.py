b44b2d7725f5aa46ae685480b7c74d1d9b96b126
Rich <richard.a.cairns@gmail.com>
2021-11-24 20:03:21 +0000

    add scale definitions for maj and min scales

---
+++ scales/definitions.py
@@ -0,0 +1,5 @@
+scale_definitions = {
+    # First digit is the interval from root to 2nd degree of the scale. Last digit is root.
+    "maj": [2, 2, 1, 2, 2, 2, 1],
+    "min": [2, 1, 2, 2, 1, 2, 2],
+}
