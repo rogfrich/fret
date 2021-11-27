1a78ccc4bd2a5babe43aaecae83dc173fd62e450
Rich <richard.a.cairns@gmail.com>
2021-11-27 18:12:47 +0000

    add features to calculate scales based on a defined formula (major only for now)

--- scales/definitions.py
+++ scales/definitions.py
@@ -1,5 +1,3 @@
 scale_definitions = {
-    # First digit is the interval from root to 2nd degree of the scale. Last digit is root.
-    "maj": [2, 2, 1, 2, 2, 2, 1],
-    "min": [2, 1, 2, 2, 1, 2, 2],
+    "major": [0, 2, 4, 5, 7, 9, 11],
 }
