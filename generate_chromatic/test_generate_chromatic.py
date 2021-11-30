ee7c78288eaa5615f326afa2e453b4d761ce74c8
Rich <richard.a.cairns@gmail.com>
2021-11-30 17:20:16 +0000

    blacken code

--- generate_chromatic/test_generate_chromatic.py
+++ generate_chromatic/test_generate_chromatic.py
@@ -1,6 +1,44 @@
 from generate_chromatic import cs
 
+
 def test_cs():
-    correct = ['d', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#']
-    generated = cs('d')
+    correct = [
+        "d",
+        "d#",
+        "e",
+        "f",
+        "f#",
+        "g",
+        "g#",
+        "a",
+        "a#",
+        "b",
+        "c",
+        "c#",
+        "d",
+        "d#",
+        "e",
+        "f",
+        "f#",
+        "g",
+        "g#",
+        "a",
+        "a#",
+        "b",
+        "c",
+        "c#",
+        "d",
+        "d#",
+        "e",
+        "f",
+        "f#",
+        "g",
+        "g#",
+        "a",
+        "a#",
+        "b",
+        "c",
+        "c#",
+    ]
+    generated = cs("d")
     assert generated == correct
--- scales/test_scales.py
+++ scales/test_scales.py
@@ -1,15 +1,55 @@
 from scales import Scale
 
+
 def test_name():
-    s = Scale('major', 'g')
-    assert s.name == 'G major scale'
+    s = Scale("major", "g")
+    assert s.name == "G major scale"
+
 
 def test_chromatic_scale_starting_at_root():
-    s = Scale('major', 'g')
-    correct = ['g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#', 'g', 'g#', 'a', 'a#', 'b', 'c', 'c#', 'd', 'd#', 'e', 'f', 'f#']
+    s = Scale("major", "g")
+    correct = [
+        "g",
+        "g#",
+        "a",
+        "a#",
+        "b",
+        "c",
+        "c#",
+        "d",
+        "d#",
+        "e",
+        "f",
+        "f#",
+        "g",
+        "g#",
+        "a",
+        "a#",
+        "b",
+        "c",
+        "c#",
+        "d",
+        "d#",
+        "e",
+        "f",
+        "f#",
+        "g",
+        "g#",
+        "a",
+        "a#",
+        "b",
+        "c",
+        "c#",
+        "d",
+        "d#",
+        "e",
+        "f",
+        "f#",
+    ]
     assert s.chromatic_scale_starting_at_root == correct
 
+
 def test_notes_in_scale():
-    s = Scale('major', 'g')
-    correct = ['g', 'a', 'b', 'c', 'd', 'e', 'f#']
+    s = Scale("major", "g")
+    correct = ["g", "a", "b", "c", "d", "e", "f#"]
     assert s.notes_in_scale == correct
